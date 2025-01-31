f579a2aff335cfbba9036a0531ba5c875d34dcd9
Shreyas Janai <sheryasjanai@gmail.com>
2025-01-31 20:38:29 +0530

    Create PrimeNumbers.java

---
+++ PrimeNumbers.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    
+    
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    
+        System.out.print("Enter the first number: ");
+        int start = scanner.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int end = scanner.nextInt();
+        
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+        
+        
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        
+        scanner.close();
+    }
+}
